ebc8997256cb5a44fbdc84a586b423ca9b815b39
Guilhem Martin Saint Léon <guilhem.msl@gmail.com>
2022-11-30 19:07:55 +0100

    books search-bar implemented

--- src/components/Nav/NavMain.jsx
+++ src/components/Nav/NavMain.jsx
@@ -1,28 +1,54 @@
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import useAuth from "../../auth/useAuth";
 import "../../styles/NavMain.css";
 
 const NavMain = () => {
-	const { isLoggedIn, currentUser, removeUser } = useAuth();
-	return (
-		<nav className="NavMain">
-			<NavLink className="logo" to="/">
-				App name
-			</NavLink>
-			{isLoggedIn && (
-				<>
-					<NavLink to="/profile">{currentUser && currentUser.email}</NavLink>
-					<button onClick={removeUser}>Log-Out</button>
-				</>
-			)}
-			{!isLoggedIn && (
-				<>
-					<NavLink to="/signin">Log in</NavLink>
-					<NavLink to="/signup">Sign Up</NavLink>
-				</>
-			)}
-		</nav>
-	);
+  const [searchQuery, setSearchQuery] = useState("");
+  const { isLoggedIn, currentUser, removeUser } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate(`/search?books=${searchQuery}`);
+  }
+  return (
+    <nav className="NavMain">
+      <NavLink className="logo" to="/">
+        <h1>Alexandria 🏡</h1>
+      </NavLink>
+      {isLoggedIn && (
+        <>
+          <NavLink to="/profile">{currentUser && currentUser.email}</NavLink>
+          <button onClick={removeUser}>Log-Out</button>
+        </>
+      )}
+      {!isLoggedIn && (
+        <>
+          <NavLink to="/signin">Log in</NavLink>
+          <NavLink to="/signup">Sign Up</NavLink>
+        </>
+      )}
+
+      <form className="nav-search" onSubmit={handleSubmit}>
+        <label htmlFor="search">
+          <img src="/images/icon-search.png" alt="magnifying glass" />
+        </label>
+        <input
+          className="input-height"
+          type="search"
+          name="searchBooks"
+          placeholder="Chercher un livre"
+          value={searchQuery}
+          onChange={(event) => {
+            setSearchQuery(event.target.value);
+          }}
+          id="search"
+        />
+        <button className="search-btn">Chercher</button>
+      </form>
+    </nav>
+  );
 };
 
 export default NavMain;
