d65a39c885ed63c080ca9113221c97aa85d59c0e
chamsdinn <chams86@msn.com>
2022-12-02 15:36:51 +0100

    random books

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
+import service from "../api/apiHandler";
+import { useState, useEffect } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 
 const Home = () => {
+  const [someBooks, setSomeBooks] = useState([])
+
+  useEffect(() => {
+    service.get('/api').then((response) => {
+      setSomeBooks(response.data)
+    });
+  }, []);
+
   return (
     <div>
       <h1>Bienvenue sur la LIBRIOTHÈQUE</h1>
@@ -9,6 +20,23 @@ const Home = () => {
         Bibliothèque en accès libre proposant plus de 3000 livres numériques
         gratuits en langue française.
       </h2>
+      <ul className="found-books">
+        {someBooks.map((book, index) => (
+          <Link to={`/books/${book._id}`}>
+            <li key={book._id}>
+              <h3>{book.title}</h3>
+
+              <h4>
+                {book.author ? book.author.name : "Nom de l'auteur non précisé"}
+              </h4>
+              {/* {console.log(book.authors[0].name, "index:", index)} */}
+              <img src={book.formats["image/jpeg"]} alt={book.title} />
+              <hr />
+            </li>
+          </Link>
+        ))}
+      </ul>
+
       <Footer />
     </div>
   );
