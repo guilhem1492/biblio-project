dc878fdfb037ac61f7c3054d6bb9b36a40ec17e3
Guilhem Martin Saint Léon <guilhem.msl@gmail.com>
2022-12-05 20:25:18 +0100

    home page styled

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,23 +20,27 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <h1>Bienvenue sur la LIBRIOTHÈQUE</h1>
-      <h2>
-        Bibliothèque en accès libre proposant plus de 3000 livres numériques
-        gratuits en langue française.
-      </h2>
+    <div className="home">
+      <h1>Bienvenue à la LIBRIOTHÈQUE</h1>
+      <p className="intro">
+        Bibliothèque en accès libre proposant plus de 3400 ebooks gratuits en
+        langue française.
+      </p>
       <ul className="found-books">
         {someBooks.map((book, index) => (
           <Link key={book.id} to={`/books/${book._id}`}>
             <li>
-              <h3>{book.title}</h3>
+              {/* <h3>{book.title}</h3> */}
 
-              <h4>
-                {book.author ? book.author.name : "Nom de l'auteur non précisé"}
-              </h4>
               {/* {console.log(book.authors[0].name, "index:", index)} */}
-              <img src={book.formats["image/jpeg"]} alt={book.title} />
+              <img
+                className="found-cover"
+                src={book.formats["image/jpeg"]}
+                alt={book.title}
+              />
+              {/* <h4>
+                {book.author ? book.author.name : "Nom de l'auteur non précisé"}
+              </h4> */}
             </li>
           </Link>
         ))}
